545c89f4e7054e81fbd60cd49dac9a0347b5c642
liebespaar93 <liebespaar93@gmail.com>
2023-07-21 04:16:16 +0900

    wordpress backup set

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -243,9 +243,9 @@ ft_set_database() {
 	EOSQL
 	mysql_ready "'root'@'localhost' user change password $MYSQL_ROOT_PASSWORD"
 
-	if [ docker_process_sql --database=wordpress <<<'SELECT 1' != "1" ] ; then
+	if [ -z docker_process_sql --database=wordpress <<<'SELECT 1' ] ; then
 		docker_process_sql wordpress < /conf/wordpress_backup.sql
-		mysql_ready "wordpress_backup"
+		mysql_ready "wordpress_backup /conf/wordpress_backup.sql "
 	fi
 }
 _main()
